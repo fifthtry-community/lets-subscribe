b40f0962782c2dba93cc540edba5f798d6d26239
siddhantCodes <siddhantk232@gmail.com>
2024-06-21 17:28:51 +0530

    impl `/set-tracker/?t=`
    this processor will drop in a tracker id and a session id if based on
    the value of `t` query param

---
+++ src/set_tracker.rs
@@ -0,0 +1,54 @@
+/// ### Set Tracker: `/set-tracker/?t=<encrypted/hashed uid>`
+/// create a tracker cookie with the user_id decrypted from `t`
+///
+// if there's a ?t then set session to tracking user id
+// if there's no ?t and the sid.0 is None then create a new session
+#[ft_sdk::processor]
+fn set_tracker(
+    mut conn: ft_sdk::Connection,
+    // t is the encrypted user_id (i64)
+    ft_sdk::Query(t): ft_sdk::Query<"t", Option<String>>,
+    sid: ft_sdk::Cookie<{ ft_sdk::auth::SESSION_KEY }>,
+    host: ft_sdk::Host,
+) -> ft_sdk::processor::Result {
+    // set the tracking cookie
+    let session_id = match t {
+        Some(t) => Some(create_session_using_t(t, &mut conn)?),
+        None => {
+            if sid.0.is_none() {
+                Some(ft_sdk::session::SessionID::new(&mut conn, None, None)?)
+            } else {
+                // session is already set, do nothing
+                None
+            }
+        }
+    };
+
+    let mut resp = ft_sdk::processor::json(serde_json::json!({ "success": true }))?;
+
+    if let Some(session_id) = session_id {
+        resp = resp.with_cookie(subscription::session_cookie(&session_id.0, host.clone())?);
+    }
+
+    resp = resp.with_cookie(subscription::tracker_cookie(
+        &ft_sdk::utils::uuid_v8(),
+        host,
+    )?);
+
+    Ok(resp)
+}
+
+/// create tracker entry using the user id decrypted and parsed from `t`
+/// `t` is the encrypted user_id (i64)
+fn create_session_using_t(
+    t: String,
+    conn: &mut ft_sdk::Connection,
+) -> Result<ft_sdk::session::SessionID, ft_sdk::Error> {
+    let user_id: ft_sdk::PlainText =
+        ft_sdk::EncryptedString::from_already_encrypted_string(t).try_into()?;
+
+    let user_id = user_id.to_string().parse::<i64>()?;
+    let user_id = ft_sdk::auth::UserId(user_id);
+
+    Ok(ft_sdk::session::SessionID::new(conn, Some(user_id), None)?)
+}

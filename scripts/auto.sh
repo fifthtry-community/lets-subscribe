bff64aa297f733cc1e844932d03280a3c32347a9
siddhantCodes <siddhantk232@gmail.com>
2025-03-22 13:40:19 +0530

    Development scripts and packages setup
    Setup:
    - lets-subscribe.fifthtry.site (main package)
    - lets-subscribe-template.fifthtry.site (example website using main pkg)
    - lets-subscribe.fifthtry-community.com (docs reference etc for main
      pkg)
    
    Add auto.sh and other scripts to `run-ui`, `run-www` and `run-template`
    etc.

---
+++ scripts/auto.sh
@@ -0,0 +1,59 @@
+# shellcheck disable=SC2155
+export PROJ_ROOT=$(pwd)
+FASTN=${FASTN_BINARY:-fastn}
+
+function pushd2() {
+    PUSHED=$(pwd)
+    cd "${PROJDIR}""$1" >> /dev/null || return
+}
+
+function popd2() {
+    cd "${PUSHED:-$PROJDIR}" >> /dev/null || return
+    unset PUSHED
+}
+
+function run-ui() {
+    pushd2 "${PROJ_ROOT}/lets-subscribe.fifthtry.site" || return 1
+    echo "Using $FASTN to serve lets-subscribe.fifthtry.site/"
+    $FASTN serve --offline
+    popd2
+}
+
+function run-www() {
+    pushd2 "${PROJ_ROOT}/lets-subscribe.fifthtry-community.com" || return 1
+    echo "Using $FASTN to serve lets-subscribe.fifthtry-community.com/"
+    $FASTN serve --offline
+    popd2
+}
+
+function update-ui() {
+    pushd2 "${PROJ_ROOT}/lets-subscribe.fifthtry.site" || return 1
+    $FASTN update
+    popd2
+}
+
+function update-www() {
+    pushd2 "${PROJ_ROOT}/lets-subscribe.fifthtry-community.com" || return 1
+    $FASTN update
+    popd2
+}
+
+function update-template() {
+    pushd2 "${PROJ_ROOT}/lets-subscribe-template.fifthtry.site" || return 1
+    $FASTN update
+    popd2
+}
+
+function build-wasm() {
+    pushd2 "${PROJ_ROOT}" || return 1
+    # this script should be used both for local development and for building on ci
+    sh scripts/build-wasm.sh
+    popd2
+}
+
+function run-template() {
+    pushd2 "${PROJ_ROOT}/lets-subscribe-template.fifthtry.site" || return 1
+
+    build-wasm
+    $FASTN --trace serve --port 9093 --offline
+}
